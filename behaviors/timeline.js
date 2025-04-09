37b14dd72a6ecca659d65a12b8243b1e589cd54b
Tessa Walsh <tessa@bitarchivist.net>
2025-04-09 06:18:51 -0400

    Add timeline.js custom behavior (#2)
    * Add initial draft timeline behavior
    
    * Fix iframe wait and add wait for media iframes
    
    * Better handle iframes
    
    * Yield number of slides
    
    * Yield initial slides to see if working
    
    * Fix next selector
    
    * Wait at least 5 seconds
    
    * fix timeline behavior, running directly in iframe (needs browsertrix-behaviors 0.8.1)
    
    * Update behavior
    
    * Wait three seconds on slide
    
    * Move some log messages to debug
    
    * Add 1 second wait when moving backwards through slides
    
    * Wait two seconds
    
    * Add comment with page behavior was developed for
    
    ---------
    
    Co-authored-by: Ilya Kreymer <ikreymer@gmail.com>

---
+++ behaviors/timeline.js
@@ -0,0 +1,60 @@
+// Developed for https://phd.aydeethegreat.com/a-timeline-of-campus-community-and-national-events-new/
+
+class Timeline
+{
+  static id = "TimelineJS";
+
+  static runInIframe = true;
+
+  static isMatch() {
+    return window.location.href.startsWith("https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html");
+  }
+
+  static init() {
+    return {};
+  }
+
+  async* run(ctx) {
+    const { log, Lib, autofetcher } = ctx;
+
+    yield log("Waiting for page to finish loading", "debug");
+    await Lib.awaitLoad();
+
+    // Click on previous as necessary until we're at first slide
+    do {
+      const previous = document.querySelector("button.tl-slidenav-previous");
+      if (!previous || !previous.checkVisibility()) {
+        break;
+      }
+
+      log("Moving to previous slide until first slide is reached");
+      previous.click();
+      await Lib.sleep(1000);
+    } while(true);
+
+    // Click on next until we're at last slide
+    do {
+      const next = document.querySelector("button.tl-slidenav-next");
+      if (!next || !next.checkVisibility()) {
+        break;
+      }
+
+      log("Waiting 2 seconds on new slide", "debug");
+      await Lib.sleep(2000);
+
+      const mediaIframe = document.querySelector("iframe.tl-media-item");
+      if (mediaIframe) {
+        log("Waiting for embedded media content to load", "debug");
+        try {
+          await Lib.awaitLoad(mediaIframe);
+        } catch (e) {}
+        await Lib.sleep(2000);
+      }
+
+      yield Lib.getState(ctx, "Moving to next slide", "slides");
+      next.click();
+      await Lib.sleep(1000);
+      //await Lib.waitForNetworkIdle();
+    } while(true);
+  }
+}

75df203da58834ae404d722133678a6bea1e230b
Tessa Walsh <tessa@bitarchivist.net>
2025-12-14 20:17:58 -0500

    Try to match View inner text with document.evaluate

--- behaviors/unt-health.js
+++ behaviors/unt-health.js
@@ -14,7 +14,8 @@ class UNTHealthBehavior
     const { log, Lib, autofetcher } = ctx;
     let click = 0;
 
-    for await (const elem of document.querySelectorAll("button[ng-reflect-router-link]")) {
+    for await (const elem of document.evaluate("//button[contains(., 'View)]", document, null, XPathResult.ANY_TYPE, null)) {
+    // for await (const elem of document.querySelectorAll("button[ng-reflect-router-link]")) {
       elem.click();
       click++;
       yield Lib.getState(ctx, "Clicked on profile view button", "click");

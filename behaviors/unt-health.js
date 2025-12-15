aae62cfa3065c7034c560883ebaa894504136468
Tessa Walsh <tessa@bitarchivist.net>
2025-12-14 19:55:03 -0500

    Add WIP UNT Health behavior

---
+++ behaviors/unt-health.js
@@ -0,0 +1,23 @@
+class UNTHealthBehavior
+{
+  static id = "UNT Health";
+
+  static isMatch() {
+    return window.location.href === "https://profiles.unthsc.edu/home";
+  }
+
+  static init() {
+    return {};
+  }
+
+  async* run(ctx) {
+    const { log, Lib, autofetcher } = ctx;
+    let click = 0;
+
+    for await (const elem of document.querySelectorAll("button[ng-reflect-router-link]")) {
+      elem.click();
+      click++;
+      yield Lib.getState("Clicked on profile view button", "click");
+    }
+  }
+}

9acb4ec7e35af9ca299ee0e998e7762226b35e6b
Tessa Walsh <tessa@bitarchivist.net>
2025-12-14 20:37:09 -0500

    Skip buttons that don't have View inner text

--- behaviors/unt-health.js
+++ behaviors/unt-health.js
@@ -14,8 +14,11 @@ class UNTHealthBehavior
     const { log, Lib, autofetcher } = ctx;
     let click = 0;
 
-    for await (const elem of document.evaluate("//button[contains(., 'View)]", document, null, XPathResult.ANY_TYPE, null)) {
-    // for await (const elem of document.querySelectorAll("button[ng-reflect-router-link]")) {
+    for await (const elem of document.querySelectorAll("button[ng-reflect-router-link]")) {
+      if !(elem.innerText && elem.innerText === "View") {
+        continue;
+      }
+
       elem.click();
       click++;
       yield Lib.getState(ctx, "Clicked on profile view button", "click");

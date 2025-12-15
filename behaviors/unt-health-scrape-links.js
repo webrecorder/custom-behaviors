64a47c6507e3fd07754dc23c756a34ae0d082c34
Tessa Walsh <tessa@bitarchivist.net>
2025-12-15 18:18:25 -0500

    Add new behavior to scrape links from View buttons

---
+++ behaviors/unt-health-scrape-links.js
@@ -0,0 +1,40 @@
+class UNTHealthBehavior
+{
+  static id = "UNT Health Scrape Links";
+
+  static isMatch() {
+    return window.location.href === "https://profiles.unthsc.edu/home";
+  }
+
+  static init() {
+    return {};
+  }
+
+  async* run(ctx) {
+    const { log, Lib, autofetcher } = ctx;
+
+    const baseUrl = "https://profiles.unthsc.edu";
+
+    for await (const elem of document.querySelectorAll("button[ng-reflect-router-link]")) {
+      if (elem.innerText && elem.innerText === "View") {
+        
+        // Grab data from attribute
+        const profileData = elem.getAttribute("ng-reflect-router-link");
+        if (!profileData) {
+          continue;
+        }
+
+        // Construct URL from that data
+        const dataArray = profileData.split(",");
+        if (dataArray.length !== 2) {
+          continue;
+        }
+        const urlToQueue = `${baseUrl}${dataArray[0]}/${dataArray[1]}`;
+
+        // Queue URL
+        await Lib.addLink(urlToQueue);
+        yield Lib.getState(ctx, `Queued new URL: ${urlToQueue}`);
+      }
+    }
+  }
+}

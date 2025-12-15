c86fe929f182e10e296da1183e57d5cc183d9757
Tessa Walsh <tessa@bitarchivist.net>
2025-12-14 21:37:58 -0500

    Add code to return to page

--- behaviors/unt-health.js
+++ behaviors/unt-health.js
@@ -14,10 +14,30 @@ class UNTHealthBehavior
     const { log, Lib, autofetcher } = ctx;
     let click = 0;
 
+    const origHref = self.location.href;
+
     for await (const elem of document.querySelectorAll("button[ng-reflect-router-link]")) {
       if (elem.innerText && elem.innerText === "View") {
         elem.click();
         click++;
+
+        // wait a bit
+        await new Promise(r => setTimeout(r, 2000));
+
+        // if we navigated to new page, go back
+        if (self.location.href != origHref) {
+          await new Promise((resolve) => {
+            window.addEventListener(
+              "popstate",
+              () => {
+                resolve(null);
+              },
+              { once: true },
+            );
+
+            window.history.back();
+          });
+        }
         yield Lib.getState(ctx, "Clicked on profile view button", "click");
       }
     }

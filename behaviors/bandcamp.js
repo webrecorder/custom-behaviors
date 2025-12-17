58e4f4d02ed4f85eca28e2fb53fdc2891963d9a9
Tessa Walsh <tessa@bitarchivist.net>
2025-12-16 20:30:09 -0500

    Add functionality to load more comments

--- behaviors/bandcamp.js
+++ behaviors/bandcamp.js
@@ -10,16 +10,39 @@ class BandcampReleaseBehavior
     return {};
   }
 
+  static countWritings() {
+    const writings = document.querySelectorAll("div.writing");
+  }
+
   async* run(ctx) {
     const { Lib } = ctx;
 
+    // click all "play track" buttons to ensure we get audio
     for await (const elem of document.querySelectorAll("div.play_status")) {
       elem.click();
 
       // wait two seconds to give audio time to start
       await Lib.sleep(2000);
 
-      await Lib.getState(ctx, "Clicked play track button", "clicks");
+      await Lib.getState(ctx, "Clicked play track button", "click");
+    }
+
+    // click "more" to load more comments, wait until they load,
+    // then repeat
+    const initialWritingsCount = this.countWritings();
+
+    while(true) {
+      const moreWritingsBtn = document.querySelector("a.more-writing");
+      moreWritingsBtn.click();
+
+      await Lib.sleep(2000);
+
+      await Lib.getState(ctx, "Click more writings button", "click");
+
+      const newWritingsCount = this.countWritings();
+      if (initialWritingsCount === newWritingsCount) {
+        break;
+      }
     }
   }
 }

4cfc77ceaa6ccdc9e240c8f59d6988220db4c661
Tessa Walsh <tessa@bitarchivist.net>
2025-12-16 23:28:22 -0500

    Wait til track progress bar starts moving, load comments before screenshots

--- behaviors/bandcamp.js
+++ behaviors/bandcamp.js
@@ -1,3 +1,8 @@
+// Generic bandcamp behavior that:
+// - Loads all sponsored comments before text extraction/screenshots
+// - Iterates through track list and starts each track, waiting for
+// each to begin before moving on
+
 class BandcampReleaseBehavior
 {
   static id = "Bandcamp Release";
@@ -11,8 +16,37 @@ class BandcampReleaseBehavior
   }
 
   countWritings() {
-    const writings = document.querySelectorAll("div.writing");
-    return writings.length || 0;
+    return document.querySelectorAll("div.writing").length;
+  }
+
+  // Load all reviews before moving on to text extraction, screenshots, behaviors
+  async awaitPageLoad(ctx) {
+    const { Lib } = ctx;
+
+    let lastWritingsCount = this.countWritings();
+
+    while(true) {
+      const moreWritingsBtn = document.querySelector("a.more-writing");
+      if (!moreWritingsBtn) {
+        break;
+      }
+
+      moreWritingsBtn.click();
+      ctx.log({msg: "Clicked more writings button"});
+
+      await Lib.sleep(3000);
+
+      const newWritingsCount = this.countWritings();
+      ctx.log({msg: "New writings count", count: newWritingsCount});
+
+      if (newWritingsCount === lastWritingsCount) {
+        ctx.log({msg: "No new writings loaded, page ready"});
+        break;
+      }
+
+      lastWritingsCount = newWritingsCount;
+    }
+
   }
 
   async* run(ctx) {
@@ -23,36 +57,33 @@ class BandcampReleaseBehavior
       elem.click();
 
       // wait two seconds to give audio time to start
-      await Lib.sleep(2000);
+      // await Lib.sleep(2000);
+
+      // wait until track is playing
+      const maxAttempts = 10
+      let attempts = 0
+      while(true) {
+        if (attempts >= maxAttempts) {
+          break;
+        }
+        attempts++;
+
+        const trackProgressBar = document.querySelector("div.progbar_empty div.thumb");
+        try {
+          const left = trackProgressBar.style.left;
+          if (left && left > "0px") {
+            ctx.log({msg: "Track started!"})
+            break;
+          }
+        } catch(e) {}
+
+        await Lib.sleep(500);
+      }
+
+      // wait one second just for good measure
+      await Lib.sleep(1000);
 
       yield Lib.getState(ctx, "Clicked play track button");
-    }
-
-    // click "more" to load more comments, wait until they load, then repeat
-    let lastWritingsCount = this.countWritings();
-    ctx.log({msg: "Initial writings count", count: lastWritingsCount});
-
-    while(true) {
-      const moreWritingsBtn = document.querySelector("a.more-writing");
-      if (!moreWritingsBtn) {
-        break;
-      }
-
-      moreWritingsBtn.click();
-
-      await Lib.sleep(4000);
-
-      yield Lib.getState(ctx, "Click more writings button");
-
-      const newWritingsCount = this.countWritings();
-      ctx.log({msg: "New writings count", count: newWritingsCount});
-
-      if (newWritingsCount === lastWritingsCount) {
-        ctx.log({msg: "No new writings loaded, quitting"});
-        break;
-      }
-
-      lastWritingsCount = newWritingsCount;
     }
   }
 }
